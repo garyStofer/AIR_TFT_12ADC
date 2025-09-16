dfb83c698304cf6c0d614c09bc0a0af3587d9765
garyStofer <garyStofer@gmail.com>
2025-09-16 11:20:11 -0700

    added defines for different models display boards  and for SD-Card code exclusion

--- build_opts.h
+++ build_opts.h
@@ -1,10 +1,8 @@
 /* This file controls the build-time features */
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  BUILD OPTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 #include "BoardPins.h"
-// for serial debug
-//#define DEBUG
-#define N_NUMBER "N9169K"
-	
-// see BoardPins.h for pins	
 
-// #define MENUWRAP   // allows the menu to wrap around from last to first enntry etc..
+#define N_NUMBER "N9169K"
+#define WITH_SD_CARD	
+#define LCD_ROTATION 3 // for red pcb
+//#define LCD_ROTATION 1 // for blue pcb
